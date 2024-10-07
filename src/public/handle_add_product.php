7fd9e6a3169e33c506f82f1955cb21849ffe4c3e
Чингис <Underchina@mail.ru>
2024-10-07 21:48:52 +0800

    Отредактирован предыдущий комит

--- src/public/handle_add_product.php
+++ src/public/handle_add_product.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-
+$userId = $_SESSION['user_id'];
 function validate()
 {
     $errors = [];
@@ -10,77 +10,54 @@ function validate()
 
     } else {
         $errors['product_id'] = "Поле product_id должно быть заполнено";
+        $pdo = new PDO('pgsql:host=postgres;port=5432;dbname=mydb', 'user', 'pass');
+        $stmt = $pdo->prepare("SELECT id FROM products WHERE id = :product_id;");
+        $stmt->execute();
+        $products = $stmt->fetchAll();
+        if($products === false){
+            $errors['product_id'] = "Нет такого id";
+        }
     }
 
     if (isset($_POST['amount'])) {
         $amount = $_POST['amount'];
     }
 
-
-    $pdo = new PDO('pgsql:host=postgres;port=5432;dbname=mydb', 'user', 'pass');
-    $stmt = $pdo->prepare("SELECT id FROM products");
-    $stmt->execute();
-    $products = $stmt->fetchAll();
-    $product_id = $_POST['product_id'];
-    $flag = true;
-
-    foreach ($products as $product) {
-        $id = $product['id'];
-        //print_r($id);
-        //echo "\n";
-        //print_r($product_id);
-        //echo "\n";
-        if($product_id == $id) {
-            $flag = false;
-            print_r($flag);
-            break;
-        }
-
-    }
-    if($flag) {
-        $errors['product_id'] = "Нет такого id";
-    }
     return $errors;
+
 }
+
+
+$amount = $_POST['amount'];
 $productId = $_POST['product_id'];
-$userId = $_SESSION['user_id'];
 $pdo = new PDO("pgsql:host=postgres; port=5432; dbname=mydb", "user", "pass");
 $stmt=$pdo->prepare('SELECT * FROM user_products WHERE user_id = :user AND product_id = :product');
 $stmt->execute(['user'=>$userId, 'product'=>$productId]);
 $res = $stmt->fetch();
-$amount = $res['amount'];
 
-function addProduct()
+
+function addProduct($productId, $amount, $userId)
 {
-    $amount= $_POST['amount'];
-
-    $amount += $_POST['amount'];
-    $productId = $_POST['product_id'];
     $pdo = new PDO('pgsql:host=postgres;port=5432;dbname=mydb', 'user', 'pass');
-    $userId = $_SESSION['user_id'];
     $stmt = $pdo->prepare("INSERT INTO user_products (user_id, product_id, amount) VALUES (:user_id, :product_id, :amount)");
     $stmt->execute(['user_id' => $userId, 'product_id' => $productId, 'amount' => $amount]);
 }
 
 
-function addRepeatProduct($amount)
+function addRepeatProduct($productId ,$amount, $userId)
 {
     $errors = validate();
     if(empty($errors)) {
-        $amount += $_POST['amount'];
-
-
-        $productId = $_POST['product_id'];
-        $userId = $_SESSION['user_id'];
         $pdo = new PDO('pgsql:host=postgres;port=5432;dbname=mydb', 'user', 'pass');
         $stmt = $pdo->prepare("UPDATE user_products SET amount = :amount WHERE user_id = :user_id AND product_id = :product_id");
         $stmt->execute(['user_id' => $userId, 'product_id' => $productId, 'amount' => $amount]);
     }
 }
 if($res === false){
-    addProduct();
+    addProduct($productId, $amount, $userId);
 }else{
-    addRepeatProduct($amount);
+    $amount += $res['amount'];
+    addRepeatProduct($productId ,$amount, $userId);
 }
 header('Location: /basket');
 
