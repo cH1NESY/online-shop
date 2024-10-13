8df9fb309b4b1f548eb345f20ef062ecdfd5c7b2
Чингис <underchina@mail.ru>
2024-10-13 17:08:57 +0800

    daada

---
+++ src/Model/OrderProduct.php
@@ -0,0 +1,37 @@
+<?php
+
+class OrderProduct
+{
+    public function getByUserIdAndByProductId(int $userId, int $productId): array
+    {
+        $pdo = new PDO("pgsql:host=postgres; port=5432; dbname=mydb", "user", "pass");
+        $stmt = $pdo->prepare('SELECT * FROM order_products WHERE user_id = :userId AND product_id = :productId');
+        $stmt->execute(['userId' => $userId, 'productId' => $productId]);
+        $res = $stmt->fetch();
+        return $res;
+    }
+
+    public function addProductInBasket(int $userId,int $productId,int $amount)
+    {
+        $pdo = new PDO('pgsql:host=postgres;port=5432;dbname=mydb', 'user', 'pass');
+        $stmt = $pdo->prepare("INSERT INTO order_products (user_id, product_id, amount) VALUES (:userId, :productId, :amount)");
+        $stmt->execute(['userId' => $userId, 'productId' => $productId, 'amount' => $amount]);
+    }
+
+    public function updateAmount(int $userId,int $productId,int $amount)
+    {
+        $pdo = new PDO('pgsql:host=postgres;port=5432;dbname=mydb', 'user', 'pass');
+        $stmt = $pdo->prepare("UPDATE order_products SET amount = :amount WHERE user_id = :user_id AND product_id = :product_id");
+        $stmt->execute(['user_id' => $userId, 'product_id' => $productId, 'amount' => $amount]);
+    }
+
+    public function getProductsByUserId(int $userId): array
+    {
+        $pdo = new PDO('pgsql:host=postgres;port=5432;dbname=mydb', 'user', 'pass');
+        $stmt = $pdo->prepare("SELECT * FROM order_products 
+        JOIN products ON user_products.product_id = products.id WHERE user_id = :user_id");
+        $stmt->execute(['user_id' => $userId]);
+        $res = $stmt->fetchAll();
+        return $res;
+    }
+}
